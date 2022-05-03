0146d7a9d496a7803bf4c144ac74a2bac9c0b21d
lucasnathan <lucas.nathan80@hotmail.com>
2022-05-03 11:09:36 -0300

    trying multi region

--- main.tf
+++ main.tf
@@ -5,13 +5,18 @@ terraform {
       version = "~> 3.27"
     }
   }
-
-  required_version = ">= 0.14.9"
 }
 
 provider "aws" {
-  profile = "default"
+  alias = "virginia"
+
   region  = "us-east-1"
+}
+
+provider "aws" {
+  alias           = "saopaulo"
+
+  region          = "sa-east-1"
 }
 
 resource "aws_instance" "iot-sensors" {
@@ -21,5 +26,35 @@ resource "aws_instance" "iot-sensors" {
 
   tags = {
     Name = "iot-${count.index}"
+  }
+}
+
+resource "aws_instance" "firewall" {
+  ami           = "ami-0f9fc25dd2506cf6d"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ids"
+  }
+}
+
+resource "aws_instance" "fog" {
+  ami           = "ami-0f9fc25dd2506cf6d"
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name = "logstash"
+  }
+}
+
+module "cloud" {
+  source            = "./modules/ec2"
+  ami           = "ami-0f9fc25dd2506cf6d"
+  instance_type = "t2.micro"
+  providers = {
+    aws = aws.saopaulo
+  }
+  tags = {
+    Name = "elasticsearch"
   }
 }
